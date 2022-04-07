2d71b2d1c28e599fe494bfe12aa52fceb08bb149
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-07 12:40:51 +0600

    successfully create google login permission by popup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app);
 
 function App() {
 
   const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
-    console.log("working");
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      console.log("error dicche:", error);
+    })
   }
 
   return (
